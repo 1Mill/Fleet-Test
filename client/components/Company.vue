80b6f3e229ccfee745b44bdbe5c6f0b51c923806
1Mill <erik.ekberg@outlook.com>
2018-10-15 20:15:41 -0700

    Render companies on front-end

---
+++ client/components/Company.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>{{ company.name }}</h2>
+	</div>
+</template>
+
+
+<script>
+export default {
+	props: {
+		company: {
+			type: Object,
+			required: true
+		}
+	}
+}
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,26 @@
 <template>
 	<div>
-		Index
+		<h1>Fleet Test</h1>
+
+		<Company
+		v-for='company in companies' :key='company.name'
+		:company='company'
+		/>
 	</div>
 </template>
+
+<script>
+import Company from '~/components/Company'
+
+export default {
+	async asyncData ({ app }) {
+		const providers = await app.$axios.$get(`/providers`)
+		const shippers = await app.$axios.$get(`/shippers`)
+
+		return { companies: providers.concat(shippers) }
+	},
+	components: {
+		Company
+	}
+}
+</script>
